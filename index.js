e3476d3983f3e918a114a59372923d141e0772f1
Frog.Dev <frog.dev.studio@gmail.com>
2018-05-04 02:32:38 +0700

    update ssl

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ MongoClient.connect(MONGODB_URL, function(err, client) {
 });
 
 const sslOptions = {
-    key: fs.readFileSync('yolostudio_xyz.cer'),
+    key: fs.readFileSync('yolostudio_xyz.p7b'),
     cert: fs.readFileSync('yolostudio_xyz.crt'),
     ca : fs.readFileSync('yolostudio_xyz.ca-bundle')
   };
